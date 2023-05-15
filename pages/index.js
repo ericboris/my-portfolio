ad3146696fdd026fec626db12624380f0356a086
ericboris <ericboris@tutanota.com>
2023-05-15 12:34:30 -0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import AboutMe from '../components/AboutMe';
+
+export default function Home() {
+  return (
+    <div>
+      <AboutMe />
+      {/* Import other components in similar fashion */}
+    </div>
+  )
+}
