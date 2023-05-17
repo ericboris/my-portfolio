47a63be8911c9f1f8c1390bc72abee971f5dda20
ericboris <ericboris@tutanota.com>
2023-05-17 15:12:40 -0700

    remove unnecessary components

--- app/page.js
+++ app/page.js
@@ -1,15 +1,9 @@
 import Header from "../components/Header";
-import AboutMe from "../components/AboutMe";
-import Contact from "../components/Contact";
-import Projects from "../components/Projects";
-import Stack from "../components/Stack";
-import Tools from "../components/Tools";
-import Footer from "../components/Footer";
-import Education from "../components/Education";
-import Blog from "../components/Blog";
-import Skills from "../components/Skills";
-import References from "../components/References";
 import HorizontalLine from "../components/HorizontalLine";
+import AboutMe from "../components/AboutMe";
+import Projects from "../components/Projects";
+import Education from "../components/Education";
+import Footer from "../components/Footer";
 
 export default function Home() {
     return (
