c0ab58cb98dbef9d9ab2c916bdf90aa402ec8d25
ericboris <ericboris@tutanota.com>
2023-05-17 09:31:22 -0700

    initial header style

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,48 @@
 import React from 'react';
 
-const Header = () => (
-    <div className="sticky top-2 bg-jaune shadow text-raisin-black text-2xl px-4 py-1 flex justify-between w-full">
-        <div>
-            <h1 className="font-bold">
-                <a href="http://localhost:3000/" target="_blank" rel="noopener noreferrer">Home</a>
-            </h1>
-        </div>
+const Header = () => {
+    const links = [
+        {
+            title: 'Github',
+            link: 'https://github.com/ericboris',
+        },
+        {
+            title: 'Linkedin',
+            link: 'https://www.linkedin.com/in/eric-boris/',
+        },
+        {
+            title: 'Resume',
+            link: 'https://www.linkedin.com/in/eric-boris/',
+        },
+    ];
 
-        <div>
-            <ul className="flex">
-                <li className="mr-1.5 hover:underline decoration-solid text-gray-700 line-through pointer-events-none">
-                    <a href="http://localhost:3000/" target="_blank" rel="noopener noreferrer">Blog</a>
-                </li>
-                <li className="ml-1.5 hover:underline decoration-solid">
-                    <a href="https://github.com/ericboris" target="_blank" rel="noopener noreferrer">Github</a>
-                </li>
-            </ul>
+    return (
+        <div className="sticky flex h-10 items-center bg-white top-0 font-black justify-between w-full text-decoration-none">
+            <h1>
+                <a
+                    href="http://localhost:3000/" 
+                    className="text-2xl mx-1 hover:text-sky-600" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                >
+                    Eric Boris
+                </a>
+            </h1>
+
+            <div className="flex">
+                {links.map(({ title, link }) => (
+                    <a 
+                        className="mx-1 hover:text-sky-600"
+                        href={link} 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        {title}
+                    </a>
+                ))}
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Header;
