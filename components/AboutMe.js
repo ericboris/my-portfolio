ce747403889b1fbbe4bff3ce9839c7a97054e381
ericboris <ericboris@tutanota.com>
2023-05-17 09:31:44 -0700

    initial about me style

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 const AboutMe = () => (
-    <div className="bg-raisin-black shadow py-10 md:py-20 px-4 text-left text-3xl md:text-5xl">
-        <h2 className="text-5xl text-jaune">
-            &#128075; Hi there, I'm Dummy Text.
-        </h2>
-            This is a dummy paragraph. It's used as placeholder text.
-            Dummy text has been used since the 1500s.
-            It's used to fill space on pages to give an idea of what the final content will look like.
-        <div className="text-xl text-pink">
-            This is another dummy paragraph. It's also used as placeholder text.
+    <div className="flex my-5">
+        <div className="flex-initial basis-1/2 mx-5 max-w-xs">
+            <img src="/image01.png" alt="Profile" />
+        </div>
+        <div className="basis-1/2 mx-5 font-black">
+            <h2 className="text-4xl text-orange-500">
+                Hello, World!
+            </h2>
+            <p className="text-l">
+                I'm Eric, a software engineer based in Seattle.
+            </p>
         </div>
     </div>
 );
