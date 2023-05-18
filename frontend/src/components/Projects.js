a360bc54202851f58569e3a28b026d3b15dc5892
ericboris <ericboris@tutanota.com>
2023-05-18 11:09:06 -0700

    Add homepage Projects carousel

--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ArrowRightIcon } from '@heroicons/react/24/solid';
 import Project from './Project';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const Projects = () => {
     const projects = [
@@ -11,13 +13,24 @@ const Projects = () => {
             image: "/helix.png",
             link: "https://github.com/ericboris/aura-backend",
         },
+        {
+            company_title: "Geometry2",
+            company_subtitle: "Decentralized Exchange",
+            description: "Reimagined trading on the ethereum blockchain.",
+            image: "/helix.png",
+            link: "https://github.com/ericboris/aura-backend",
+        },
     ];
 
     return (
-        <div className="mx-4 border">
-            {projects.map((project, index) => (
-                <Project key={index} project={project} />
-            ))}
+        <div className="border">
+            <Carousel showThumbs={false} showStatus={false}>
+                {projects.map((project, index) => (
+                    <div key={index}>
+                        <Project project={project} />
+                    </div>
+                ))}
+            </Carousel>
         </div>
     );
 };
