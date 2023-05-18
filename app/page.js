a811aaaafc94515d9828f0779ec889d889308903
ericboris <ericboris@tutanota.com>
2023-05-17 18:46:36 -0700

    remove Education components from main page

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 import HorizontalLine from "../components/HorizontalLine";
 import AboutMe from "../components/AboutMe";
 import Projects from "../components/Projects";
-import Education from "../components/Education";
+import Educations from "../components/Educations";
 import Footer from "../components/Footer";
 
 export default function Home() {
@@ -19,12 +19,10 @@ export default function Home() {
         <Header />
 
         <AboutMe />
+
         <HorizontalLine />
-        
 
         <Projects/>
-        <HorizontalLine />
-        <Education />
 
         <Footer />
         </main>
