94e636f8f65a6f0a21364beb430c7757551bb592
ericboris <ericboris@tutanota.com>
2023-05-18 12:06:51 -0700

    Stick Header to the top of the window

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -24,7 +24,7 @@ const Header = () => {
     };
 
     return (
-        <nav className="bg-white relative">
+        <nav className="sticky top-0 bg-white relative">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex items-center">
